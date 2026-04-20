a8f036b4494f2c24fa3629c0d316fd5ceb0b38f2
branRoberts <brandonkrob12@gmail.com>
2026-04-20 11:18:23 -0400

    Third Commit

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -6,28 +6,41 @@ public class FamousQuotes {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-
+       boolean process = true;
         try {
-            String[] quotes = new String[10];
-            quotes[0] = "\"Don't be sorry. Be better.\" Kratos";
-            quotes[1] = "\"What is a man? A miserable little pile of secrets.\"  Dracula";
-            quotes[2] = "\"Snake? Snake? SNAKE!\" Metal Gear Solid";
-            quotes[3] = "\"Do a barrel roll!\" Star Fox 64 Kratos";
-            quotes[4] = "\"Don't be sorry. Be better.\" Kratos";
-            quotes[5] = "\"You Have Died\" Dark Souls";
-            quotes[6] = "\"War…war never changes\" Fallout";
-            quotes[7] = "\"A man chooses; a slave obeys.\" Bioshock";
-            quotes[8] = "\"No Russian.\" Call of Duty: Modern Warfare 2";
-            quotes[9] = "\"What is a man but the sum of his memories? We are the stories we live,\n the tales we tell ourselves.\" Assassin’s Creed: Revelations";
+            while (process) {
+                String[] quotes = new String[10];
+                quotes[0] = "\"Don't be sorry. Be better.\" Kratos";
+                quotes[1] = "\"What is a man? A miserable little pile of secrets.\"  Dracula";
+                quotes[2] = "\"Snake? Snake? SNAKE!\" Metal Gear Solid";
+                quotes[3] = "\"Do a barrel roll!\" Star Fox 64 Kratos";
+                quotes[4] = "\"Don't be sorry. Be better.\" Kratos";
+                quotes[5] = "\"You Have Died\" Dark Souls";
+                quotes[6] = "\"War…war never changes\" Fallout";
+                quotes[7] = "\"A man chooses; a slave obeys.\" Bioshock";
+                quotes[8] = "\"No Russian.\" Call of Duty: Modern Warfare 2";
+                quotes[9] = "\"What is a man but the sum of his memories? We are the stories we live,\n the tales we tell ourselves.\" Assassin’s Creed: Revelations";
 
-            System.out.println("Enter a number 1 - 10:");
-            int number = input.nextInt() - 1;
-            System.out.println(quotes[number]);
+                System.out.println("Enter a number 1 - 10:");
+                int number = input.nextInt() - 1;
+                input.nextLine();
+                System.out.println(quotes[number]);
 
+                if (number >= 10 || number < 0) {
+                    continue;
+                }
+                System.out.println("Would you like to repeat the process? Yes or No");
+                String confirm = input.nextLine();
+                if (confirm.equalsIgnoreCase("Yes")) {
+
+                }
+                else {
+                    process = false;
+                }
+            }
         } catch (Exception e) {
-            System.out.println("Something went wrong");
+            System.out.println("something went wrong");
         }
-
 
     }
 }
